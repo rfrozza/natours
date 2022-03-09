e29209101da7f7ffae81130cdc553a8570090a2d
Renan Frozza <renan.apc@gmail.com>
2022-03-09 06:07:33 -0300

    ESLint configuration

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+dotenv.config({ path: './config.env' });
+
+//console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
