b7f73bc567c1cf35fb98880a44800a1e626ded1b
Renan Frozza <renan.apc@gmail.com>
2022-03-17 07:20:50 -0300

    Protecting routes, handling token errors and validation

--- models/userModel.js
+++ models/userModel.js
@@ -45,6 +45,9 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
     select: false,
   },
+  passwordChangedAt: {
+    type: Date,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -62,6 +65,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    //console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
+  }
+  // False = Password not changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
